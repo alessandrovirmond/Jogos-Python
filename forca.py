ce037feb21c5832ad77fc645eb60fac7d9bc2818
Alessandro <avirmond2000@gmail.com>
2021-07-05 17:40:26 -0300

    atualizando o jogo forca

--- forca.py
+++ forca.py
@@ -4,6 +4,20 @@ def jogar_forca ():
     print("Bem vindo ao jogo da foca")
     print("********************************")
 
+    palavra_secreta = "banana"
+    acertou = False
+    enforcou = False
+
+    while (not enforcou and not acertou):
+        chute = input("Qual a letra?")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.uper() == letra.uper()):
+                print("Ëncontrei a letra {} na posicao {}".format(letra,index))
+            index = index + 1
+        print("jogando...")
 
     print("fim do jogo")
 
