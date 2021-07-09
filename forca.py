105492b3d6e0cc83b0681e047f842f95d44fd452
Alessandro <avirmond2000@gmail.com>
2021-07-08 22:00:53 -0300

    jogo com encerramento e palavra secreta responsiva

--- forca.py
+++ forca.py
@@ -4,26 +4,39 @@ def jogar_forca ():
     print("Bem vindo ao jogo da foca")
     print("********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_","_","_","_","_","_"]
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     acertou = False
     enforcou = False
+    erros = 0
 
-    print(palavra_acertada)
+    print(letras_acertadas)
 
     while (not enforcou and not acertou):
 
         chute = input("Qual a letra?")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(6 - erros))
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
 
         print(letras_acertadas)
+
+    if (acertou):
+        print("Voce ganhou.Parabens!!!")
+    else:
+        print("Voce perdeu")
 
     print("fim do jogo")
 
